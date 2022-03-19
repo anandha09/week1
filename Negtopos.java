f37e5a2c2f53fcfb3b79d88621c0436d4b37ee78
anandha09 <101935694+anandha09@users.noreply.github.com>
2022-03-19 21:03:42 +0530

    Add files via upload

---
+++ Negtopos.java
@@ -0,0 +1,11 @@
+package week1.day1.assignments;
+
+public class Negtopos {
+	 public static void main(String args[]){    
+		 int i=-20;
+		 if(i<0){
+			 i=i*-1;
+			 System.out.println(i);
+		 }
+	 }
+	 }
---
+++ postneg.java
@@ -0,0 +1,20 @@
+package week1.day1.assignments;
+
+public class postneg {
+	
+	 public static void main(String args[]){   
+		 int i=-35;
+		 if(i==0)
+		 {
+			 System.out.println("The number is neither negative or positive");
+		 }
+		 if(i<0) {
+			 System.out.println("The number is negative");
+		 }
+		 else {
+			 System.out.println("The number is positive");
+		 }
+		 
+	 }
+
+}
